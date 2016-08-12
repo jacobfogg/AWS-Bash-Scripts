35a67c02d93c588722d6bfef4e205f861b7a5ae9
Jacob Fogg <jacob@datajoe.com>
2016-08-12 11:59:34 -0600

    Changed to self compiling nginx and openssl in order to add the correct compile flag to nginx for supporting ALPN. I also added several variables for easier updating of the script.

--- Centos7-HTTP2-Nginx-PHPFPM7.sh
+++ Centos7-HTTP2-Nginx-PHPFPM7.sh
@@ -4,49 +4,50 @@
 #TODO: ping that domain somehow to ensure the DNS is configured correctly.
 # TODO: If it's not, throw and error telling the user to add the DNS record and try after propagation finishes and die.
 
-#Set some variables
+#Set some user variables
 EMAIL=jacob@datajoe.com
 DOMAIN=djoe.us
 SUBDOMAIN=www
 
+#Set some script variables
+CENTVER="7"
+OPENSSL="openssl-1.0.2h"
+NGINX="nginx-1.11.3-1"
+
 #update the server
 yum -y update
+yum clean all
 cd ~/
 
+#Add the builder user used for installing nginx
+useradd builder
+groupadd builder
+
 #install some helper tools
-yum install -y wget
-yum group install -y "Development Tools"
+yum install -y epel-release #the EPEL (Extra Packages for Enterprise Linux) Repository
+yum group install -y "Development Tools" #Install the development tools used for compiling from source
+yum install -y wget openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel pcre-devel #Install several other packages
 
-#install OpenSSL 1.0.2
-wget https://www.openssl.org/source/openssl-1.0.2h.tar.gz
-tar -zxvf openssl-1.0.2h.tar.gz -C /usr/local/src
-cd /usr/local/src/openssl-1.0.2h/
-./config
-make depend
-make install
-
-mv /bin/openssl /root/
-ln -s /usr/local/ssl/bin/openssl /bin/openssl
+#install but don't build OpenSSL 1.0.2h
+mkdir -p /opt/lib
+wget https://www.openssl.org/source/${OPENSSL}.tar.gz /opt/lib/${OPENSSL}.tar.gz
+tar -zxvf /opt/lib/${OPENSSL}.tar.gz -C /opt/lib
 
 #install Nginx
-#Create the repo file:
-echo "# /etc/yum.repos.d/nginx.repo" >> /etc/yum.repos.d/nginx.repo
-echo "[nginx]" >> /etc/yum.repos.d/nginx.repo
-echo "name=nginx repo" >> /etc/yum.repos.d/nginx.repo
-echo "baseurl=http://nginx.org/packages/mainline/centos/7/\$basearch/" >> /etc/yum.repos.d/nginx.repo
-echo "gpgcheck=0" >> /etc/yum.repos.d/nginx.repo
-echo "enabled=1" >> /etc/yum.repos.d/nginx.repo
+rpm -ivh http://nginx.org/packages/mainline/centos/${CENTVER}/SRPMS/${NGINX}.el${CENTVER}.centos.gnx.src.rpm #download the RPM
+sed -i "s|--with-http_ssl_module|--with-http_ssl_module --with-openssl=/opt/lib/${OPENSSL}|g" /root/rpmbuild/SPECS/nginx.spec #modify the config to add reference to the newer openssl
+rpmbuild -ba /root/rpmbuild/SPECS/nginx.spec #compile nginx
+rpm -ivh /root/rpmbuild/RPMS/x86_64/${NGINX}.el${CENTVER}.ngx.x86_64.rpm #install it
+rpm -ivh /root/rpmbuild/RPMS/x86_64/${NGINX}.el${CENTVER}.centos.ngx.x86_64.rpm
 
-yum install -y nginx #install nginx
-
-#TODO: update the djoe.us portion to use the domain entry from above
 mkdir -p /var/www/${DOMAIN} #create the webroot where the SSL will be installed
 
 #Configure the new directory to be compatible with selinux
 semanage fcontext -a -t usr_t "/var/www/${DOMAIN}(/.*)?"
 restorecon -Rv /var/www/${DOMAIN}
 
-sed -i '0,/\/usr\/share\/nginx\/html/s//\/var\/www\/${DOMAIN}/' /etc/nginx/conf.d/default.conf #Change root location for webroot
+cp /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.old
+sed -i '0,/\/usr\/share\/nginx\/html/s//\/var\/www\/'${DOMAIN}'/' /etc/nginx/conf.d/default.conf #Change root location for webroot
 
 echo "Welcome to nginx" > /var/www/${DOMAIN}/index.html #Create an index.html file for nginx to render
 
@@ -56,14 +57,13 @@ systemctl enable nginx #Enable nginx so it will start on boot
 
 
 #install the secure Cert:
-yum install -y epel-release #the EPEL (Extra Packages for Enterprise Linux) Repository
 yum install -y certbot #Certbot itself
 #TODO: modify the command below to use the previously captured domain
 certbot certonly --webroot -m ${EMAIL} -w /var/www/${DOMAIN} -d ${DOMAIN} -d ${SUBDOMAIN}.${DOMAIN} #Request the SSL using CertBot
 
 sed -i 's/listen       80;/listen       80;\n    listen       443 default_server ssl http2;/' /etc/nginx/conf.d/default.conf
-sed -i 's/server_name  localhost;/server_name  localhost;\n    ssl_certificate_key   \/etc\/letsencrypt\/live\/${SUBDOMAIN}.${DOMAIN}\/privkey.pem;/' /etc/nginx/conf.d/default.conf
-sed -i 's/server_name  localhost;/server_name  localhost;\n    ssl_certificate       \/etc\/letsencrypt\/live\/${SUBDOMAIN}.${DOMAIN};/' /etc/nginx/conf.d/default.conf
+sed -i 's/server_name  localhost;/server_name  localhost;\n    ssl_certificate_key   \/etc\/letsencrypt\/live\/'${SUBDOMAIN}'.'${DOMAIN}'\/privkey.pem;/' /etc/nginx/conf.d/default.conf
+sed -i 's/server_name  localhost;/server_name  localhost;\n    ssl_certificate       \/etc\/letsencrypt\/live\/'${SUBDOMAIN}'.'${DOMAIN}'\/fullchain.pem;/' /etc/nginx/conf.d/default.conf
 
 
 
@@ -84,8 +84,10 @@ systemctl restart nginx #Restart the sever to update the settings
 #List of websites that helped:
 # Installing OpenSSL1.0.2
 # - http://thelinuxfaq.com/403-how-to-install-openssl-1-0-2d-version-on-centos-6-centos-7-rhel
-# Installing Nginx mainline repo:
+# Installing Nginx:
 # - http://m12.io/blog/http-2-with-haproxy-and-nginx-guide
+# - https://forum.nginx.org/read.php?2,261880,261951#msg-261951
+# - https://gist.github.com/kennwhite/6b6250e635c45c92a118a7a5cdc052c6
 # Configuring selinux to work with the new webroot
 # - http://www.terminalinflection.com/relocating-apache-selinux/
 # Using letsencrypt with nginx
